0e5fb65a639789d2b3737387e2e10bbebbe7ff66
Jimmy <bruno_pier@hotmail.com>
2019-12-05 00:21:14 -0300

    First step to Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
-
+import Footer from './components/Footer';
 
 
 function App() {
-
+      const fecha = new Date().getFullYear();
 return (
   <Fragment>
-  <Header
+      <Header
     titulo='Tienda Virtual'
-  />
+      />
+      <Footer
+      fecha={fecha}
+      />
   </Fragment>
   );
 }
